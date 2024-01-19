cbe88520b58ca1d9a835c1bd5cf047a7a57f3dd2
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-19 21:59:14 +0530

    write signup logic

--- app/src/main/java/com/pp/shortvideo/SignupActivity.kt
+++ app/src/main/java/com/pp/shortvideo/SignupActivity.kt
@@ -2,10 +2,38 @@ package com.pp.shortvideo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import com.pp.shortvideo.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.submitBtn.setOnClickListener {
+            signup()
+        }
+    }
+    fun signup() {
+        val email = binding.emailInput.text.toString()
+        val password = binding.passwordInput.text.toString()
+        val confirmPassword = binding.confirmPasswordInput.text.toString()
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.emailInput.setError("Email not valid")
+            return;
+        }
+        if(password.length<6){
+            binding.passwordInput.setError("Minimum 6 character")
+            return
+        }
+        if(password!=confirmPassword){
+            binding.confirmPasswordInput.setError("Password not matched")
+            return
+        }
+        signupWithFirebase(email,password)
+    }
+    fun signupWithFirebase(email : String, password : String) {
+
     }
 }
