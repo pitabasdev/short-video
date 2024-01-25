df579dbac7d28fc751596e1a5e7a05282f363cf2
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-25 18:20:46 +0530

    update singlevideo logic

--- app/src/main/java/com/pp/shortvideo/SingleVideoPlayerActivity.kt
+++ app/src/main/java/com/pp/shortvideo/SingleVideoPlayerActivity.kt
@@ -2,10 +2,47 @@ package com.pp.shortvideo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.pp.shortvideo.adapter.VideoListAdapter
+import com.pp.shortvideo.databinding.ActivitySingleVideoPlayerBinding
+import com.pp.shortvideo.databinding.ProfileVideoItemVideoBinding
+import com.pp.shortvideo.model.VideoModel
 
 class SingleVideoPlayerActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySingleVideoPlayerBinding
+    lateinit var videoId: String
+    lateinit var adapter: VideoListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single_video_player)
+        binding=ActivitySingleVideoPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        videoId=intent.getStringExtra("videoId")!!
+        setViewPager()
+
     }
+    fun setViewPager(){
+        val options=FirestoreRecyclerOptions.Builder<VideoModel>()
+            .setQuery(
+                Firebase.firestore.collection("videos")
+                    .whereEqualTo("videoId",videoId),
+                VideoModel::class.java
+            ).build()
+        adapter= VideoListAdapter(options)
+        binding.videPager.adapter=adapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        adapter.stopListening()
+    }
+
 }
